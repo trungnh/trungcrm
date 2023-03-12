4013f7482e26facff768ed6ef92df78d93170ae5
Trung, Nguyen Huu <trungnh28@gmail.com>
2023-03-12 16:02:53 +0700

    Update report filter

--- resources/views/admin/report/components/list.blade.php
+++ resources/views/admin/report/components/list.blade.php
@@ -67,7 +67,7 @@
             <!-- Light table -->
             <div class="table-responsive">
                 <table class="table align-items-center table-flush">
-                    <thead class="thead-light">
+                    <thead class="thead-dark">
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col" class="sort" data-sort="name">Tháng</th>
