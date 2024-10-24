435a3e6d55b865efc4511e4f121bec00888f507e
Trung, Nguyen Huu <trungnh28@gmail.com>
2024-10-24 23:46:01 +0700

    Add table daily report to home

--- resources/views/admin/home/content/table.blade.php
+++ resources/views/admin/home/content/table.blade.php
@@ -21,6 +21,8 @@
                         <th class="header" scope="col" :class="{'invisible': hideByRole}">Tổng chi</th>
                         <th class="header" scope="col">Tiền COD</th>
                         <th class="header" scope="col" :class="{'invisible': hideByRole}">Lợi nhuận</th>
+                        <th class="header" scope="col" :class="{'invisible': hideByRole}">%Ads</th>
+                        <th class="header" scope="col" :class="{'invisible': hideByRole}">ROAS</th>
                     </tr>
                     </thead>
                     <tbody class="list table-striped">
@@ -28,41 +30,41 @@
                     <tr v-for="(item, index) in thisMonthReportItemsTable">
                         <th scope="row">{{ item.date }}</th>
                         <th scope="row" class="row-orders">
-                            <span class="editable" @click="enableEditOrder(index)">
+                            <span class="editable">
                                 {{ item.orders || 0}}
                             </span>
-                            <input @blur="resetEditFields(index)" type="text" v-model="item.orders" class="form-control" aria-describedby="basic-addon3">
                         </th>
                         <td scope="row" class="row-qty">
-                            <span class="editable" @click="enableEditQty(index)">
+                            <span class="editable">
                                 {{ item.product_qty || 0}}
                             </span>
-                            <input @blur="resetEditFields(index)" type="text" v-model="item.product_qty" class="form-control" aria-describedby="basic-addon3">
                         </td>
                         <td scope="row" @click="resetEditFields(index)" :class="{'invisible': hideByRole}">
                             <span>{{ formatCurrencyNumber(item.totalUnitPrice) }}</span>
                         </td>
                         <td scope="row" class="row-ads">
-                            <span class="editable" @click="enableEditAds(index)">{{ formatCurrencyNumber(item.ads_amount)}}
-                            </span>
-                            <input @blur="calculateAdsAmount(index, this)" type="text" class="form-control" aria-describedby="basic-addon3">
+                            <span class="editable">{{ formatCurrencyNumber(item.ads_amount)}}</span>
                         </td>
-                        <td scope="row" @click="resetEditFields(index)" :class="{'invisible': hideByRole}">
+                        <td scope="row" :class="{'invisible': hideByRole}">
                             <span>{{ formatCurrencyNumber(item.totalShippingPrice) }}</span>
                         </td>
-                        <td scope="row" @click="resetEditFields(index)" :class="{'invisible': hideByRole}">
+                        <td scope="row" :class="{'invisible': hideByRole}">
                             <span>{{ formatCurrencyNumber(item.totalReturnPrice) }}</span>
                         </td>
-                        <td scope="row" @click="resetEditFields(index)" :class="{'invisible': hideByRole}">
+                        <td scope="row" :class="{'invisible': hideByRole}">
                             <span>{{ formatCurrencyNumber(item.totalSpent) }}</span>
                         </td>
                         <td scope="row" class="row-revenue">
-                            <span class="editable text-green font-weight-bold" @click="enableEditRevenue(index)">{{ formatCurrencyNumber(item.revenue)}}
-                            </span>
-                            <input @blur="resetEditFields(index)" type="text" v-model="item.revenue" class="form-control" aria-describedby="basic-addon3">
+                            <span class="editable text-green font-weight-bold">{{ formatCurrencyNumber(item.revenue)}}</span>
                         </td>
-                        <td scope="row" @click="resetEditFields(index)" :class="{'invisible': hideByRole}">
+                        <td scope="row" :class="{'invisible': hideByRole}">
                             <span class="text-red font-weight-bold">{{ formatCurrencyNumber(item.profit) }}</span>
+                        </td>
+                        <td scope="row" :class="{'invisible': hideByRole}">
+                            <span>{{ formatNumber((item.ads_amount/item.revenue)*100) }}</span>
+                        </td>
+                        <td scope="row" :class="{'invisible': hideByRole}">
+                            <span class="font-weight-bold">{{ formatNumber(item.revenue/item.ads_amount) }}</span>
                         </td>
                     </tr>
                     @endverbatim
